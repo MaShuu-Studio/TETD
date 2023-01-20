d01e2d082b1ea75b2366af04fdebd0fd2fb8aa41
Shuu <ghwls328@naver.com>
2023-01-21 00:43:28 +0900

    Tower Random Prob

--- Assets/Scripts/Data/TowerManager.cs
+++ Assets/Scripts/Data/TowerManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using EnumData;
 using Data;
 
 public static class TowerManager
@@ -9,6 +10,7 @@ public static class TowerManager
     private static string path = Application.streamingAssetsPath + "/Data/";
 
     private static Dictionary<int, Tower> towers;
+    private static List<int>[,] egTowerIds;
     public static List<int> Keys { get { return keys; } }
     private static List<int> keys;
 
@@ -16,11 +18,16 @@ public static class TowerManager
     {
         towers = new Dictionary<int, Tower>();
         List<TowerData> list = DataManager.DeserializeListJson<TowerData>(path, "Tower");
+        egTowerIds = new List<int>[EnumArray.Elements.Length, EnumArray.Grades.Length];
+        for (int i = 0; i < EnumArray.Elements.Length; i++)
+            for (int j = 0; j < EnumArray.Grades.Length; j++)
+                egTowerIds[i, j] = new List<int>();
 
         foreach (var data in list)
         {
             Tower tower = new Tower(data);
             towers.Add(tower.id, tower);
+            egTowerIds[(int)tower.element, (int)tower.grade].Add(tower.id);
             SpriteManager.AddSprite<Tower>(data.imgsrc, tower.id, data.pivot, data.pixelperunit);
         }
         keys = towers.Keys.ToList();
@@ -35,5 +42,14 @@ public static class TowerManager
         if (towers.ContainsKey(id)) return towers[id];
 
         return null;
+    }
+
+    public static Tower GetRandomTower(Element element, Grade grade)
+    {
+        int count = egTowerIds[(int)element, (int)grade].Count;
+        int rand = Random.Range(0, count);
+        int id = egTowerIds[(int)element, (int)grade][rand];
+
+        return GetTower(id);
     }
 }
--- Assets/Scripts/UI/Shop.cs
+++ Assets/Scripts/UI/Shop.cs
@@ -1,10 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EnumData;
+using TMPro;
 
 public class Shop : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI probText;
     [SerializeField] private List<TowerInfoItem> items;
+
+    private Dictionary<Grade, float> prob;
+
+    private void Awake()
+    {
+        prob = new Dictionary<Grade, float>();
+        prob.Add(Grade.NORMAL, 50);
+        prob.Add(Grade.RARE, 80);
+        prob.Add(Grade.HEROIC, 95);
+        prob.Add(Grade.LEGENDARY, 100);
+
+        probText.text =
+            "NORMAL: " + prob[Grade.NORMAL] + "%\n" +
+            "RARE: " + (prob[Grade.RARE] - prob[Grade.NORMAL]) + "%\n" +
+            "HEROIC: " + (prob[Grade.HEROIC] - prob[Grade.RARE]) + "%\n" +
+            "LEGENDARY: " + (prob[Grade.LEGENDARY] - prob[Grade.HEROIC]) + "%";
+    }
 
     public void RerollAll()
     {
@@ -15,14 +35,17 @@ public class Shop : MonoBehaviour
     }
     public void Reroll(TowerInfoItem item)
     {
-        // 확률에 따른 랜덤수치 적용 예정
-        int amount = TowerManager.Keys.Count;
+        float rand = Random.Range(0, 100f);
+        Grade grade = Grade.NORMAL;
+        if (rand <= prob[Grade.NORMAL]) grade = Grade.NORMAL;
+        else if (rand <= prob[Grade.RARE]) grade = Grade.RARE;
+        else if (rand <= prob[Grade.HEROIC]) grade = Grade.HEROIC;
+        else grade = Grade.LEGENDARY;
 
-            int rand = Random.Range(0, amount);
+        Element element = (Element) Random.Range(0, EnumArray.Elements.Length);
 
-            int id = TowerManager.Keys[rand];
-            Tower tower = TowerManager.GetTower(id);
+        Tower tower = TowerManager.GetRandomTower(element, grade);
 
-            item.SetData(tower);
+        item.SetData(tower);
     }
 }
