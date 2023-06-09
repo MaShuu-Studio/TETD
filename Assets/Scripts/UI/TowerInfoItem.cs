8c254ef238061de555e61f5ed929cb4a61ed5d31
Shuu <ghwls328@naver.com>
2023-06-09 09:37:54 +0900

    Automate Element Dropdown on Shop

--- Assets/Scripts/UI/TowerInfoItem.cs
+++ Assets/Scripts/UI/TowerInfoItem.cs
@@ -16,13 +16,29 @@ public class TowerInfoItem : TowerInfoCard
     public void Init()
     {
         selectedElement = -1;
-        if (elementDropdown != null) elementDropdown.value = 0;
+        if (elementDropdown != null)
+        { 
+            elementDropdown.value = 0;
+
+            List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
+            for (int i = 0; i < EnumArray.Elements.Length; i++)
+            {
+                Element e = EnumArray.Elements[i];
+                string oName = EnumArray.ElementStrings[e].ToUpper();
+                Sprite oSprite = SpriteManager.GetSprite($"Element{i}");
+                TMP_Dropdown.OptionData option = new TMP_Dropdown.OptionData(oName, oSprite);
+                options.Add(option);
+            }
+            elementDropdown.AddOptions(options);
+            elementDropdown.template.sizeDelta = new Vector2(elementDropdown.template.sizeDelta.x, 75 * (1 + options.Count));            
+        }
     }
 
     public void SelectElement(int index)
     {
         selectedElement = index - 1;
     }
+
     public override void UpdateInfo()
     {
         base.UpdateInfo();
